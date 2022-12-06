25b5230f99ab4074753917842681ad6c7644b909
Zarania <scott.a.reinhardt@gmail.com>
2022-12-06 11:23:02 -0500

    Added day 6

--- benches/advent.rs
+++ benches/advent.rs
@@ -72,9 +72,24 @@ pub fn bench_day5_part2(c: &mut Criterion) {
     });
 }
 
+pub fn bench_day6_part1(c: &mut Criterion) {
+    let input = read_file("inputs", 6);
+    c.bench_function("day6 part1", |b| {
+        b.iter(|| day06::part_one(black_box(&input)))
+    });
+}
+
+pub fn bench_day6_part2(c: &mut Criterion) {
+    let input = read_file("inputs", 6);
+    c.bench_function("day6 part2", |b| {
+        b.iter(|| day06::part_two(black_box(&input)))
+    });
+}
+
 criterion_group!(day01, bench_day1_part1, bench_day1_part2);
 criterion_group!(day02, bench_day2_part1, bench_day2_part2);
 criterion_group!(day03, bench_day3_part1, bench_day3_part2);
 criterion_group!(day04, bench_day4_part1, bench_day4_part2);
 criterion_group!(day05, bench_day5_part1, bench_day5_part2);
-criterion_main!(day03);
+criterion_group!(day06, bench_day6_part1, bench_day6_part2);
+criterion_main!(day06);
