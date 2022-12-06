e5b3d70d194b1958c0c8710d29aedc8b655e802a
Zarania <scott.a.reinhardt@gmail.com>
2022-12-05 23:40:45 -0500

    Move int_from_bytes to library, likely will be used again

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,25 @@ pub fn read_file(folder: &str, day: u8) -> String {
     let f = fs::read_to_string(filepath);
     f.expect("could not open input file")
 }
+
+pub fn int_from_bytes<T>(s: &[u8]) -> T
+where
+    T: From<u8> + std::ops::Mul<T, Output = T> + std::ops::Add<T, Output = T>,
+{
+    s.iter().fold(T::from(0), |n, c| {
+        let r = match c {
+            b'0' => Some(T::from(0)),
+            b'1' => Some(T::from(1)),
+            b'2' => Some(T::from(2)),
+            b'3' => Some(T::from(3)),
+            b'4' => Some(T::from(4)),
+            b'5' => Some(T::from(5)),
+            b'6' => Some(T::from(6)),
+            b'7' => Some(T::from(7)),
+            b'8' => Some(T::from(8)),
+            b'9' => Some(T::from(9)),
+            _ => None,
+        };
+        n * T::from(10) + r.unwrap()
+    })
+}
--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,3 +1,5 @@
+use crate::int_from_bytes;
+
 pub fn part_one(input: &str) -> String {
     let (stacks, moves) = input.split_once("\n\n").unwrap();
 
@@ -81,28 +83,6 @@ pub fn part_two(input: &str) -> String {
             f.truncate(size);
         });
     String::from_utf8(vecs.iter().map(|list| *list.last().unwrap()).collect()).unwrap()
-}
-
-pub fn int_from_bytes<T>(s: &[u8]) -> T
-where
-    T: From<u8> + std::ops::Mul<T, Output = T> + std::ops::Add<T, Output = T>,
-{
-    s.iter().fold(T::from(0), |n, c| {
-        let r = match c {
-            b'0' => Some(T::from(0)),
-            b'1' => Some(T::from(1)),
-            b'2' => Some(T::from(2)),
-            b'3' => Some(T::from(3)),
-            b'4' => Some(T::from(4)),
-            b'5' => Some(T::from(5)),
-            b'6' => Some(T::from(6)),
-            b'7' => Some(T::from(7)),
-            b'8' => Some(T::from(8)),
-            b'9' => Some(T::from(9)),
-            _ => None,
-        };
-        n * T::from(10) + r.unwrap()
-    })
 }
 
 #[cfg(test)]
