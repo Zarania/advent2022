11239b67df4deac16f445b6d38f2a25e0f0c503e
Zarania <scott.a.reinhardt@gmail.com>
2022-12-15 21:40:03 -0500

    Added bench

--- benches/advent.rs
+++ benches/advent.rs
@@ -198,6 +198,20 @@ pub fn bench_day14_part2(c: &mut Criterion) {
     });
 }
 
+pub fn bench_day15_part1(c: &mut Criterion) {
+    let input = read_file("inputs", 15);
+    c.bench_function("day15 part1", |b| {
+        b.iter(|| day15::part_one(black_box(&input)))
+    });
+}
+
+pub fn bench_day15_part2(c: &mut Criterion) {
+    let input = read_file("inputs", 15);
+    c.bench_function("day15 part2", |b| {
+        b.iter(|| day15::part_two(black_box(&input)))
+    });
+}
+
 criterion_group!(day01, bench_day1_part1, bench_day1_part2);
 criterion_group!(day02, bench_day2_part1, bench_day2_part2);
 criterion_group!(day03, bench_day3_part1, bench_day3_part2);
@@ -212,4 +226,5 @@ criterion_group!(day11, bench_day11_part1, bench_day11_part2);
 criterion_group!(day12, bench_day12_part1, bench_day12_part2);
 criterion_group!(day13, bench_day13_part1, bench_day13_part2);
 criterion_group!(day14, bench_day14_part1, bench_day14_part2);
-criterion_main!(day14);
+criterion_group!(day15, bench_day15_part1, bench_day15_part2);
+criterion_main!(day15);
